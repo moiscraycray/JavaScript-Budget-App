cf0b1b2b9465558d861e30e143f9f5091b18ba21
olivia.mo <olivia.mo@carsguide.com.au>
2018-03-21 16:40:41 +1100

    added another method to the controller

--- app.js
+++ app.js
@@ -17,8 +17,7 @@ var UIController = (function() {
 // Global App Controller
 var controller = (function(budgetCtrl, UICrtl) {
 
-  document.querySelector('.add__btn').addEventListener('click', function() {
-
+  var ctrlAddItem = function() {
     // 1. get the field input data
 
     // 2. add the item to the budget controller
@@ -28,7 +27,9 @@ var controller = (function(budgetCtrl, UICrtl) {
     // 4. calculate the budget
 
     // 5. display the budget on the UI
-  });
+  }
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
 
   // adding keypress listener to the global document
   // 'event' parameter gets automically passed into our event handler by the browser, we don't need to do anything else like calling it; we just need to specify it in anonymous function
@@ -36,7 +37,7 @@ var controller = (function(budgetCtrl, UICrtl) {
     // console.log(event); // keyCode identifies which key was pressed; keyCodes are unique
     // older browsers don't support .keyCode so .which is a backup
     if (event.keyCode === 13 || event.which === 13) {
-      console.log('ENTER was pressed');
+      ctrlAddItem();
     }
   });
 
