990528c0505d04488e69a8c892f82c75e6226d4b
olivia.mo <olivia.mo@carsguide.com.au>
2018-03-21 16:16:34 +1100

    removed test code, added todo list

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
-// module pattern: it returns an object containing all of the functions that we want to be public. So the functions that we want to give the outside scope access to.
-// we've wrapped the entire function in IIFE for privacy
+// Budget Controller
 var budgetController = (function() {
 
-  // we cannot access variable x and add() from outside; throws error when we try
-  var x = 23;
-  var add = function(a) {
-    return x + a;
-  }
 
-  // we can access this; this is public method because we have returned it. This object is what gets assigned to the budget variable after this function returns.
-  // After all this runs, the budgetController variable is simply an object containing the method called publicTest, because that's what we returned from the budgetConroller anonymous function.
-  // publicTest can get x variables and add() even after returning it thanks to closures
-  // closures: an inner function always has access to the variables and parameters of its outer functino even after the outer function has returned.
-  // so here: the IIFE returns immediately and so it's effectively gone, but the publicTest() that we return will always have access to the x variable and add() because of closure. That's why we say that the publicTest method is public because it was return and now we can use it. variable x and add() are private because they are in the closure and therefore only the publicTest() can access them.
-  return {
-    publicTest: function(b) {
-      console.log(add(b));
-    }
-  }
 
 })();
 
+
+// UI Controller
 var UIController = (function() {
 
   // some code
 
 })();
 
-var controller = (function() {
 
-  // some code
+// Global App Controller
+var controller = (function(budgetCtrl, UICrtl) {
 
-})();
+  document.querySelector('.add__btn').addEventListener('click', function() {
+    
+    // 1. get the field input data
+
+    // 2. add the item to the budget controller
+
+    // 3. add the item to the UI
+
+    // 4. calculate the budget
+
+    // 5. display the budget on the UI
+  })
+
+})(budgetController, UIController);
