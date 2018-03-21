f778da3285874b7454ae43da0e3d799a01313698
olivia.mo <olivia.mo@carsguide.com.au>
2018-03-21 16:38:10 +1100

    added event listener for enter key

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var UIController = (function() {
 var controller = (function(budgetCtrl, UICrtl) {
 
   document.querySelector('.add__btn').addEventListener('click', function() {
-    
+
     // 1. get the field input data
 
     // 2. add the item to the budget controller
@@ -28,6 +28,16 @@ var controller = (function(budgetCtrl, UICrtl) {
     // 4. calculate the budget
 
     // 5. display the budget on the UI
-  })
+  });
+
+  // adding keypress listener to the global document
+  // 'event' parameter gets automically passed into our event handler by the browser, we don't need to do anything else like calling it; we just need to specify it in anonymous function
+  document.addEventListener('keypress', function(event) {
+    // console.log(event); // keyCode identifies which key was pressed; keyCodes are unique
+    // older browsers don't support .keyCode so .which is a backup
+    if (event.keyCode === 13 || event.which === 13) {
+      console.log('ENTER was pressed');
+    }
+  });
 
 })(budgetController, UIController);
