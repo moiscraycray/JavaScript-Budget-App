c4ed11c0091b81db52d00b0412a2b6bd409bf90e
olivia.mo <olivia.mo@carsguide.com.au>
2018-03-22 14:54:41 +1100

    added placeholder data as strings

--- app.js
+++ app.js
@@ -93,6 +93,24 @@ var UIController = (function() {
         value: document.querySelector(DOMstrings.inputValue).value
       };
     },
+
+    addListItem: function(obj, type) {
+      var = html;
+
+      // create HTML string with placeholder text
+      // we put in some % signs so it's easier to find later
+      if (type === 'inc') {
+        html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      } else if (type === 'exp') {
+        html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+      };
+
+
+      // replace the placeholder text with some actual data
+
+      // insert the HTML into the DOM
+    },
+
     // returning DOMstrings here so other functions can access the classnames
     getDOMstrings: function() {
       return DOMstrings;
