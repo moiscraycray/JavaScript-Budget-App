6addb49353b1e94815df6ac1318154f6bcaf7a8a
olivia.mo <olivia.mo@carsguide.com.au>
2018-03-22 12:41:07 +1100

    created object for storing all class names

--- app.js
+++ app.js
@@ -9,15 +9,28 @@ var budgetController = (function() {
 // UI Controller
 var UIController = (function() {
 
+  // storing all class names in this object so it's easy to change class names in html without messing up the code because we can replace the class names here which will reflect in the rest of the code
+  // We return DOMstrings below so other functions can access it
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  };
+
   // it needs to be public function because it needs to be accessible by the other controller
   return {
     getInput: function() {
       // to return all 3 variables in a function, return them as properties in an object
       return {
-        type: document.querySelector('.add__type').value, // Will be either inc or exp
-        description: document.querySelector('.add__description').value,
-        value: document.querySelector('.add__value').value
+        type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
       };
+    },
+    // returning DOMstrings here so other functions can access the classnames
+    getDOMstrings: function() {
+      return DOMstrings;
     }
   };
 
@@ -26,6 +39,8 @@ var UIController = (function() {
 
 // Global App Controller
 var controller = (function(budgetCtrl, UICtrl) {
+
+  var DOM = UICtrl.getDOMstrings();
 
   var ctrlAddItem = function() {
 
@@ -42,7 +57,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     // 5. display the budget on the UI
   }
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
   // adding keypress listener to the global document
   // 'event' parameter gets automically passed into our event handler by the browser, we don't need to do anything else like calling it; we just need to specify it in anonymous function
