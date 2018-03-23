2d15e5a2381569956ff6ebfc0f19be3fc1d26407
olivia.mo <olivia.mo@carsguide.com.au>
2018-03-23 15:31:33 +1100

    added some DOM manipulation so the UI gets updated with the budget calculations

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 // In this app, the way we structure the functions is that each function will do an individual action. There will be setter and getter functions
 
-// Budget Controller
+/* -------------------------------------------------------
+
+ Budget Controller
+
+----------------------------------------------------------*/
 var budgetController = (function() {
 
   var Expense = function(id, description, value) {
@@ -101,7 +105,7 @@ var budgetController = (function() {
         totalInc: data.totals.inc,
         totalExp: data.totals.exp,
         percentage: data.percentage
-      }
+      };
     },
 
     testing: function() {
@@ -111,8 +115,11 @@ var budgetController = (function() {
 
 })();
 
+/* -------------------------------------------------------
 
-// UI Controller
+ UI Controller
+
+----------------------------------------------------------*/
 var UIController = (function() {
 
   // storing all class names in this object so it's easy to change class names in html without messing up the code because we can replace the class names here which will reflect in the rest of the code
@@ -123,7 +130,11 @@ var UIController = (function() {
     inputValue: '.add__value',
     inputBtn: '.add__btn',
     incomeContainer: '.income__list',
-    expensesContainer: '.expenses__list'
+    expensesContainer: '.expenses__list',
+    budgetLabel: '.budget__value',
+    incomeLabel: '.budget__income--value',
+    expensesLabel: '.budget__expenses--value',
+    percentageLabel: '.budget__expenses--percentage'
   };
 
   // it needs to be public function because it needs to be accessible by the other controller
@@ -190,6 +201,21 @@ var UIController = (function() {
       fieldsArr[0].focus();
     },
 
+    displayBudget: function(obj) {
+
+      // getBudget() has all the calculated totals; we're accessing the objects from that function which was passed in as obj when we called displayBudget in the global app controller
+      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+      document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+      // we only want to display the percentage if it's greater than 0 && not -1
+      if (obj.percentage > 0) {
+        document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+      } else {
+        document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+      }
+    },
+
     // returning DOMstrings here so other functions can access the classnames
     getDOMstrings: function() {
       return DOMstrings;
@@ -199,7 +225,12 @@ var UIController = (function() {
 })();
 
 
-// Global App Controller
+
+/* -------------------------------------------------------
+
+  Global App Controller
+
+----------------------------------------------------------*/
 var controller = (function(budgetCtrl, UICtrl) {
 
   // we can call this function by creating an init() below, and returning init()
@@ -225,11 +256,10 @@ var controller = (function(budgetCtrl, UICtrl) {
     budgetCtrl.calculateBudget();
 
     // 2. Return the budget
-
     var budget = budgetCtrl.getBudget();
 
     // 3. display the budget on the UI
-    console.log(budget);
+    UICtrl.displayBudget(budget);
   };
 
 
@@ -266,6 +296,12 @@ var controller = (function(budgetCtrl, UICtrl) {
   return {
     init: function() {
       console.log('app started.');
+      UICtrl.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1
+      });
       setupEventListeners();
     }
   };
